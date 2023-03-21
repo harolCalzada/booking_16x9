0cbe95a91a15cb01de90284cae2ed12c03a16fd1
harol calzada <harolcalzada@MacBook-Pro-de-harol.local>
2023-03-20 20:44:52 -0500

    base project

---
+++ lib/widgets/button2.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:salon_app/uidata.dart';
+
+class MyButton2 extends StatelessWidget {
+  final String btnText;
+  final VoidCallback onpressed;
+
+  const MyButton2({Key key, this.btnText, this.onpressed}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //padding: EdgeInsets.symmetric(horizontal: 10),
+      height: 30,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: UIData.mainColor,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+        ),
+        onPressed: onpressed,
+        child: Text(btnText,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 15.5,
+                fontWeight: FontWeight.w400)),
+      ),
+      // child: TextButton(
+
+      //     // color: UIData.mainColor,
+      //     // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+      //     // onPressed: onpressed,
+      //     // child: Text(btnText, style: TextStyle(color: Colors.white,
+      //     // fontWeight: FontWeight.w400)),
+      //     ),
+    );
+  }
+}
