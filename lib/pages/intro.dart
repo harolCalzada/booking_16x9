b973bce284f9dd8c75dddca808b06c5c87935970
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2023-03-20 23:28:19 -0500

    Se avance el proyecto

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -29,39 +29,57 @@ class _IntroPageState extends State<IntroPage> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
-              width: MediaQuery.of(context).size.width,
+              
+              width: 260,
+          
               decoration: BoxDecoration(
                   image: DecorationImage(
                       image: AssetImage(
-                        "assets/images/logo_jaus_dorado.png",
+                        "assets/images/logo-jaus-dorado.png",
                       ),
                       fit: BoxFit.contain)),
-              height: MediaQuery.of(context).size.height * .45,
+              height: 80,
             ),
-            Text("Book your \nFavourite Stylist",
+           SizedBox(height: 90,)
+,           
+            Text("Ya tengo cuenta JAUS",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    fontWeight: FontWeight.w600,
+                    color: Color( gradientcolor),
                     height: 1.5,
                     fontSize: 22,
                     letterSpacing: 1.3)),
-            Text(
-              "Lorem Ipsum is simply a dummy text of the printing and typesetting industry",
-              textAlign: TextAlign.center,
-              style: TextStyle(height: 1.8, letterSpacing: 1.3),
-            ),
-          SizedBox(
+                  
+                  SizedBox(height: 50,),
+                  
+                    SizedBox(
                       width: 161,
                       height: 38,
-                      child: MyButton(btnText: "Ingresar",
+                      child: MyButton(btnText: "Ingresar",colorBackground:(primarycolor),colorText: gradientcolor,width: 40,height: 20,
                        onpressed: () => Navigator.of(context).pushNamed('/homePageRoute'),
                       ),
         ),
-        ]),
+           SizedBox(height: 50,),
+           
+            SizedBox(width: 180,
+              child: Text(
+                "Si es tu primera vez en JAUS.App Registrate",
+                textAlign: TextAlign.center,
+                style: TextStyle(height: 1.8, letterSpacing: 1.3,color:Color(gradientcolor)),
+              ),
+            ),
+          SizedBox(height: 50,),
+          SizedBox(
+                      width: 161,
+                      height: 38,
+                      child: MyButton(btnText: "Registrate",colorBackground:(primarycolor),colorText: gradientcolor,width: 40,height: 20,
+                       onpressed: () => Navigator.of(context).pushNamed('/homePageRoute'),
+                      ),
+        ),]),
       ),
     );
   }
