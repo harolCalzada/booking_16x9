7d0ef889e6293e53d162e2bd416d4049656d2779
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2023-03-24 14:19:07 -0500

    se mejoro el jaus

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -22,95 +22,90 @@ class _IntroPageState extends State<IntroPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-               Stack(
-          children: [
-            Positioned(
-              top: 0,
-              child:
-              Container(
-                width: 260,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage(
-                          "assets/images/logo-jaus-dorado.png",
-                        ),
-                        fit: BoxFit.contain)),
-                height: 80,
-              ),),
-              SizedBox(
-                height:250,
-              ),
-              Text("Ya tengo cuenta JAUS ",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Color(gradientcolor),
-                      height: 1.5,
-                      fontSize: 22,
-                      letterSpacing: 1.3)),
-              SizedBox(height: 14,),
+               Container(
+                 width: 260,
+                 decoration: BoxDecoration(
+                     image: DecorationImage(
+                         image: AssetImage(
+                           "assets/images/logo-jaus-dorado.png",
+                         ),
+                         fit: BoxFit.contain)),
+                 height: 80,
+               ),
+                 SizedBox(
+                   height:250,
+                 ),
+                 Text("Ya tengo cuenta JAUS ",
+                     textAlign: TextAlign.center,
+                     style: TextStyle(
+                         color: Color(gradientcolor),
+                         height: 1.5,
+                         fontSize: 22,
+                         letterSpacing: 1.3)),
+                 SizedBox(height: 14,),
 
-              TextButton(onPressed: () => Navigator.of(context).pushNamed('/home'),child: Text("Ingresar a JAUS"),),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Si es tu primera vez en",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 19,
-                    height: 1.8,
-                    color: Color(gradientcolor)
-                    
+                 TextButton(onPressed: () => Navigator.of(context).pushNamed('/home'),child: Text("Ingresar a JAUS"),),
+                 SizedBox(
+                   height: 10,
+                 ),
+                 Text(
+                   "Si es tu primera vez en",
+                   textAlign: TextAlign.center,
+                   style: TextStyle(
+                     fontSize: 19,
+                       height: 1.8,
+                       color: Color(gradientcolor)
+                       
       
-                    ),
-              ),
-            
-            Row(mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  child: Text(
-                        "JAUS.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 23,
-                          fontWeight: FontWeight.bold,
-                            color: Color(gradientcolor)
-                            
-                      
-                            ),
-                      ),
-                ),
-               Text(
-                  "App",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                      color: Color(gradientcolor)
-                      
+                       ),
+                 ),
+               
+               Row(mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   SizedBox(
+                     child: Text(
+                           "JAUS.",
+                           textAlign: TextAlign.center,
+                           style: TextStyle(
+                             fontSize: 23,
+                             fontWeight: FontWeight.bold,
+                               color: Color(gradientcolor)
+                               
+                         
+                               ),
+                         ),
+                   ),
+                  Text(
+                     "App",
+                     textAlign: TextAlign.center,
+                     style: TextStyle(
+                       fontSize: 16,
+                         color: Color(gradientcolor)
+                         
       
-                      ),
-                ),
-             
-             
-              ],
-            ),
-            
+                         ),
+                   ),
+                
+                
+                 ],
+               ),
+               
 
           SizedBox(height: 15,),
-             
-            Text(
-                  " Registrate",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    decoration: TextDecoration.underline,
-                      height: 1.8,
-                      letterSpacing: 1.3,
-                      color: Color(gradientcolor)
-                      
+                
+               Text(
+                     " Registrate",
+                     textAlign: TextAlign.center,
+                     style: TextStyle(
+                       fontSize: 20,
+                       decoration: TextDecoration.underline,
+                         height: 1.8,
+                         letterSpacing: 1.3,
+                         color: Color(gradientcolor)
+                         
       
-                      ),
-                ),]),
+                         ),
+                   ),
       ],
     )));
   }
