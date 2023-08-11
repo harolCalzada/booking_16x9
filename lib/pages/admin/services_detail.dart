c8a2ae66ecd60ba4538102f9b0c8249467de7d73
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2023-08-11 12:42:20 -0500

    se mejoro el app

--- lib/pages/admin/services_detail.dart
+++ lib/pages/admin/services_detail.dart
@@ -3,69 +3,131 @@ import 'package:go_router/go_router.dart';
 import 'package:salon_app/widgets/button2.dart';
 
 class ServicesDetail extends StatefulWidget {
-  // const ServicesDetail({key, required ServiceEntity service});
-
   @override
-  State<ServicesDetail> createState() => _ServicesDetailState();
+  _ServicesDetailState createState() => _ServicesDetailState();
 }
 
 class _ServicesDetailState extends State<ServicesDetail> {
-  bool isChecked = false;
+  String imageUrl = '';
+  double price = 0.0;
+  String iconUrl = '';
+  String serviceName = '';
+  bool? isActive = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          leading: ReturnButton(
-            icon: Icons.arrow_back,
-            iconColor: Colors.black,
-            onPressed: () {
-              context.go('/SideMenu');
-            },
-          ),
-        ),
+      appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-            child: Container(
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.white,
-                      Colors.white,
-                    ],
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomRight,
-                    stops: [0.0, 1.0],
-                    tileMode: TileMode.mirror,
+        leading: ReturnButton(
+          icon: Icons.arrow_back,
+          iconColor: Colors.black,
+          onPressed: () {
+            context.go('/SideMenu');
+          },
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.white,
+                Colors.white,
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              stops: [0.0, 1.0],
+              tileMode: TileMode.mirror,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/logo-jaus-dorado.png',
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 18),
+                child: Text(
+                  "Servicios",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                    color: Colors.black,
                   ),
                 ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        alignment: Alignment.center,
-                        child: Image.asset(
-                          'assets/images/logo-jaus-dorado.png',
-                          width: 100,
-                          height: 100,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 18),
-                        child: Text(
-                          "Servicios",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
-                            color: Colors.black,
-                          ),
-                        ),
-                      )
-                    ]))));
+                  children: [
+                    TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          imageUrl = value;
+                        });
+                      },
+                      decoration: InputDecoration(labelText: 'Imagen URL'),
+                    ),
+                    TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          price = double.tryParse(value) ?? 0.0;
+                        });
+                      },
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: 'Precio'),
+                    ),
+                    TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          iconUrl = value;
+                        });
+                      },
+                      decoration: InputDecoration(labelText: 'Icono URL'),
+                    ),
+                    TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          serviceName = value;
+                        });
+                      },
+                      decoration:
+                          InputDecoration(labelText: 'Nombre del Servicio'),
+                    ),
+                    CheckboxListTile(
+                      title: Text('Activo'),
+                      value: isActive,
+                      onChanged: (value) {
+                        setState(() {
+                          isActive = value;
+                        });
+                      },
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Guardar'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
